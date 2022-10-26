4911efda26ffad62f53f109f207c0245b8433ac4
Natasha Mann <natasha.s.mann@gmail.com>
2022-10-26 14:24:36 +0100

    added dynamo permissions

--- src/orderService/handler.ts
+++ src/orderService/handler.ts
@@ -3,8 +3,22 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 export const handle = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  if (event.httpMethod === "POST") {
+    return {
+      statusCode: 200,
+      body: "POST",
+    };
+  }
+
+  if (event.httpMethod === "GET") {
+    return {
+      statusCode: 200,
+      body: "GET",
+    };
+  }
+
   return {
     statusCode: 200,
-    body: "Success",
+    body: "GET",
   };
 };
