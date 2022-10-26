a5798f1faa5c0c9fcbc7db15616c4f7fce88a7fa
Natasha Mann <natasha.s.mann@gmail.com>
2022-10-26 14:40:31 +0100

    added handler functions for order service

--- src/orderService/handler.ts
+++ src/orderService/handler.ts
@@ -1,24 +1,71 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
+type Order = {
+  id: number;
+  billingDate: Date;
+};
+
 export const handle = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  const AWS = require("aws-sdk");
+  const docClient = new AWS.DynamoDB.DocumentClient();
+  const tableName = "ordersTable";
+
   if (event.httpMethod === "POST") {
-    return {
-      statusCode: 200,
-      body: "POST",
-    };
+    if (event.body) {
+      const data = JSON.parse(event.body) as Order;
+      console.log(data);
+
+      const params = {
+        TableName: tableName,
+        Item: data,
+      };
+
+      try {
+        await docClient.put(params).promise();
+        return {
+          statusCode: 200,
+          body: "Successfully added item",
+        };
+      } catch (err) {
+        return {
+          statusCode: 400,
+          body: `POST error: ${err}`,
+        };
+      }
+    }
   }
 
   if (event.httpMethod === "GET") {
-    return {
-      statusCode: 200,
-      body: "GET",
+    const id = event.pathParameters!.id;
+
+    if (!id) {
+      throw new Error("ID not provided");
+    }
+    const params = {
+      TableName: tableName,
+      Key: {
+        id,
+      },
     };
+
+    try {
+      const data = await docClient.get(params).promise();
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+      };
+    } catch (err) {
+      return {
+        statusCode: 400,
+        body: `GET error: ${err}`,
+      };
+    }
   }
 
   return {
-    statusCode: 200,
-    body: "GET",
+    statusCode: 400,
+    body: "UNAUTHORIZED",
   };
 };
